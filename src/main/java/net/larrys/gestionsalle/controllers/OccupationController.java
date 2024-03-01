4ce73b56029fcab42b855e74b5185198bfa42e00
larrysrakoto <larrysrakoto@gmail.com.com>
2024-03-01 09:13:11 +0300

    all controllers OK

--- src/main/java/net/larrys/gestionsalle/controllers/OccupationController.java
+++ src/main/java/net/larrys/gestionsalle/controllers/OccupationController.java
@@ -1,2 +1,32 @@
-package net.larrys.gestionsalle.controllers;public class OccupationController {
+package net.larrys.gestionsalle.controllers;
+
+import lombok.AllArgsConstructor;
+import net.larrys.gestionsalle.models.Occupation;
+import net.larrys.gestionsalle.models.Prof;
+import net.larrys.gestionsalle.repositories.OccupationRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/occupation")
+@AllArgsConstructor
+public class OccupationController {
+    private OccupationRepository occupationRepository;
+    @GetMapping(path = "")
+    public List<Occupation> getAllOccupation(){
+        return occupationRepository.findAll();
+    }
+
+    @PostMapping(path = "")
+    public void save(@RequestBody Occupation occupation){occupationRepository.save(occupation);}
+
+    @PutMapping(path = "")
+     public void update(@RequestBody Occupation occupation){
+    occupationRepository.findById(occupation.getId()).ifPresent(p -> occupationRepository.save(occupation));
+  }
+    @DeleteMapping(path = "/{id}")
+    public void delete(@PathVariable(name = "id") String id){
+        occupationRepository.findById(id).ifPresent(occupation -> occupationRepository.delete(occupation));
+    }
 }
--- src/main/java/net/larrys/gestionsalle/controllers/SalleController.java
+++ src/main/java/net/larrys/gestionsalle/controllers/SalleController.java
@@ -1,6 +1,7 @@
 package net.larrys.gestionsalle.controllers;
 
 import lombok.AllArgsConstructor;
+import net.larrys.gestionsalle.models.Prof;
 import net.larrys.gestionsalle.models.Salle;
 import net.larrys.gestionsalle.repositories.SalleRepository;
 import org.springframework.web.bind.annotation.*;
@@ -18,9 +19,19 @@ public class SalleController {
     public List<Salle> getallSalle(){
         return salleRepository.findAll();
     }
-
+    @GetMapping(path = "/{codeSalle}")
+    public Salle getSalle(@PathVariable(name = "codeSalle") String codeSalle){
+        return salleRepository.findById(codeSalle).orElse(null);
+    }
     @PostMapping(path = "")
     public void save(@RequestBody Salle salle){
-
+    }
+    @PutMapping(path = "")
+    public void update(@RequestBody Salle salle){
+        salleRepository.findById(salle.getCodeSalle()).ifPresent(p -> salleRepository.save(salle));
+    }
+    @DeleteMapping(path = "/{codeSalle}")
+    public void delete(@PathVariable(name = "codeSalle") String codeProf){
+        salleRepository.findById(codeProf).ifPresent(p -> salleRepository.delete(p));
     }
 }
